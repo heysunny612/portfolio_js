d485276de297566a503e8c0a06c709ed45aa5f95
SUNNY <heysunny612@gmail.com>
2023-04-22 00:15:00 +0900

    json데이터 테스트

--- script/todoList.js
+++ script/todoList.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const getTodos = () => {
+  fetch('http://localhost:3000/todos')
+    .then((response) => response.json())
+    .then((todos) => console.log(todos));
+};
+
+const init = () => {
+  getTodos();
+};
+
+init();
