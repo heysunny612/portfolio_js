d1be51dd5c3db5a2989c3918fa4b55cd92004bbb
SUNNY <heysunny612@gmail.com>
2023-04-28 17:49:22 +0900

    dataPicker수정_현재달에 이전달,다음달 날짜표시 및 클릭이벤트 수정

--- script/datePicker.js
+++ script/datePicker.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('.container');
 const calendar = document.querySelector('.calendar');
 const calendarYear = document.querySelector('#year');
 const calendarDays = document.querySelector('.calendar-days');
@@ -7,6 +8,7 @@ const monthList = document.querySelector('.month-list');
 const prevYearBtn = document.querySelector('#prev-year');
 const nextYearBtn = document.querySelector('#next-year');
 const selectedDay = document.querySelector('#selected_day');
+const btnCalendar = document.querySelector('#btn-calendar');
 
 const currDate = new Date();
 let currMonth = currDate.getMonth();
@@ -103,14 +105,28 @@ const createCalendar = (month, year) => {
 
   //1일 구하기
   const firstDay = new Date(year, month, 1);
-  const emptyDay = firstDay.getDay() - 1;
+  const prevEmptyDays = firstDay.getDay() - 1;
+  const lastDateOfMonth = new Date(year, month + 1, 0).getDate();
+  const lastDayOfMonth = new Date(year, month, lastDateOfMonth).getDay();
+  const lastDateOfLastMonth = new Date(year, month, 0).getDate();
 
   //일-0/월-1/화-2/수-3/목-4/금-5/토-6
   //날짜를 몇개 만들것인지에 대한 반복문
-  for (let i = 0; i <= daysOfMonth[month] + emptyDay; i++) {
+
+  //현재달에 이전달 불러오기
+  for (let i = prevEmptyDays; i >= 0; i--) {
     const day = document.createElement('li');
-    day.dataset.id = i;
+    day.classList.add('notCurrDay');
+    day.innerHTML = `${lastDateOfLastMonth - i}`;
+
+    calendarDays.appendChild(day);
+  }
+
+  //현재달 만들기
+  for (let i = 0; i <= daysOfMonth[month] + prevEmptyDays; i++) {
     if (i >= firstDay.getDay()) {
+      const day = document.createElement('li');
+      day.dataset.id = i;
       day.classList.add('day');
       day.innerHTML = `${i - firstDay.getDay() + 1}
                       <span></span>
@@ -119,15 +135,23 @@ const createCalendar = (month, year) => {
                       <span></span>`;
       //오늘찾기
       if (
-        i - emptyDay === currDate.getDate() &&
+        i - prevEmptyDays === currDate.getDate() &&
         year === currDate.getFullYear() &&
         month === currDate.getMonth()
       ) {
         day.classList.add('today');
       }
+      calendarDays.appendChild(day);
     }
+  }
+  //현재달에 다음달 부러오기
+  for (let i = lastDayOfMonth; i < 6; i++) {
+    const day = document.createElement('li');
+    day.classList.add('notCurrDay');
+    day.innerHTML = `${i - lastDayOfMonth + 1}`;
     calendarDays.appendChild(day);
   }
+
   const $days = document.querySelectorAll('.day');
 
   $days.forEach((day, index) =>
@@ -143,12 +167,38 @@ function onClickDay(index) {
   const originalSeleted = document.querySelectorAll('.selected')?.[0];
   originalSeleted?.classList.remove('selected');
   event.target.classList.add('selected');
-  selectedDay.innerHTML = `
+
+  btnCalendar.innerHTML = `
         <span class="txt">선택된 날 : </span> 
         ${currYear} 년  
         ${currMonth + 1} 월
         ${index + 1} 일
     `;
 }
+
+//캘린더 픽커 버튼 클릭
+let clicked = false;
+btnCalendar.onclick = () => {
+  if (clicked) {
+    calendar.classList.add('hide');
+  } else {
+    calendar.classList.remove('hide');
+  }
+  clicked = !clicked;
+  //console.log(clicked);
+};
+
+// 캘린더외에 영역 클릭시 닫기
+document.addEventListener('click', function (e) {
+  if (clicked == true) {
+    if (e.target.id != 'btn-calendar') {
+      let calendarDOM = e.target.closest('.calendar');
+      if (!calendarDOM) {
+        calendar.classList.add('hide');
+        return (clicked = false);
+      }
+    }
+  }
+});
 
 createCalendar(currMonth, currYear);
